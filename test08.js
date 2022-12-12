dbaac02754e0e15c5106f3f0b0b7f1b9a9b15176
step68 <mogidary23@naver.com>
2022-12-12 17:17:44 +0900

    node js

--- test08.js
+++ test08.js
@@ -1,18 +1,17 @@
 // 사용자 이벤트 처리 
 process.on('hello',function(){
-    console.log('이벤트 발생');
+    console.log('hello 이벤트 발생');
 })
-
-setTimeout(function(){
-    // console.log('3초 후에 exit 발생')
-    process.emit('hello'); // 사용자가 만드는 이벤트
-},2000)
 
 process.on('bye',function(){
     console.log('bye 이벤트 발생');
 })
 
 setTimeout(function(){
-    // console.log('3초 후에 exit 발생')
+    process.emit('hello'); // 사용자가 만드는 이벤트
+},2000)
+
+
+setTimeout(function(){
     process.emit('bye'); // 사용자가 만드는 이벤트
 },5000)
